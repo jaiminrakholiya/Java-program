1726830e0525da085d2e6484152a305a9a593c70
Jaimin Rakholiya <123720669+jaiminrakholiya@users.noreply.github.com>
2024-01-27 15:02:28 +0530

    Add files via upload

---
+++ ForLoop2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ForLoop2 {
+    public static void main(String[] args) {
+
+        for (int num = 1; num <= 5; num++) {
+            System.out.println(num);
+        }
+    }
+}
