15cef459b7d2d5ce53b8fba2121e629c6760cf5c
Jaimin Rakholiya <123720669+jaiminrakholiya@users.noreply.github.com>
2024-01-27 11:17:51 +0530

    Add files via upload

---
+++ IfDemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class IfDemo {
+    public static void main(String[] args) {
+        int i = 10;
+        if (i < 15) {
+            System.out.println("inside if block");
+            System.out.println("10 is less then 15");
+            System.out.println("I am not in if");
+        }
+    }
+
+}
---
+++ IfElaseDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class IfElaseDemo {
+    public static void main(String[] args) {
+        int i = 10;
+        if (i < 15)
+            System.out.println("i is samller then 15");
+        else
+            System.out.println("i is greater then 15");
+    }
+}
